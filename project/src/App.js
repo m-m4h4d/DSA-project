70c005bfccecdbf673aa479880b076eac8c6a272
Muhammad Mahad <132600645+m-m4h4d@users.noreply.github.com>
2023-12-13 21:59:37 +0500

    Still figuring it out

--- project/src/App.js
+++ project/src/App.js
@@ -1,9 +1,13 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 class App extends Component {
+  fetchPlaces(mapProps, map) {
+    const { google } = mapProps;
+    const service = new google.maps.places.PlacesService(map);
+  }
   render() {
     return (
       <div className="App">
@@ -11,14 +15,25 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Google Maps with React.js</h1>
         </header>
-        <Map google={this.props.google} zoom={14}>
-   
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />
-   
+        <Map
+          google={this.props.google}
+          zoom={14}
+          const
+          style={{
+            width: "70%",
+            height: "100%",
+            align: "center",
+          }}
+          onReady={this.fetchPlaces}
+          visible={true}
+        >
+          <Marker onClick={this.onMarkerClick} name={"Current location"} />
           <InfoWindow onClose={this.onInfoWindowClose}>
-              
+            <div>
+              <h1>default</h1>
+            </div>
           </InfoWindow>
+          {/* <Listing places={this.state.places} /> */}
         </Map>
       </div>
     );
@@ -26,5 +41,5 @@ class App extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: ("AIzaSyC6yyJ9x2QijsBFpyB2N0YMK_LszR9OLl4")
-})(App)
+  apiKey: "AIzaSyC6yyJ9x2QijsBFpyB2N0YMK_LszR9OLl4",
+})(App);
