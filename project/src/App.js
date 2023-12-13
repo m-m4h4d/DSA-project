4c02f0996beff2b265b1e9abb30b88472685624b
Muhammad Mahad <132600645+m-m4h4d@users.noreply.github.com>
2023-12-13 11:53:43 +0500

    Still working on it

--- project/src/App.js
+++ project/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 function App() {
   return (
@@ -18,8 +19,21 @@ function App() {
           Learn React
         </a>
       </header>
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
     </div>
   );
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: (YOUR_GOOGLE_API_KEY_GOES_HERE)
+})(App)
