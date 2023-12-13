845f15726ac669baa0533f8f4817ef36df910ba7
Muhammad Mahad <132600645+m-m4h4d@users.noreply.github.com>
2023-12-13 15:50:34 +0500

    Working google maps with API key

--- project/src/App.js
+++ project/src/App.js
@@ -1,39 +1,30 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Google Maps with React.js</h1>
+        </header>
+        <Map google={this.props.google} zoom={14}>
+   
+          <Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />
+   
+          <InfoWindow onClose={this.onInfoWindowClose}>
+              
+          </InfoWindow>
+        </Map>
+      </div>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-  apiKey: (YOUR_GOOGLE_API_KEY_GOES_HERE)
+  apiKey: ("AIzaSyC6yyJ9x2QijsBFpyB2N0YMK_LszR9OLl4")
 })(App)
